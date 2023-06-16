bab8006a71c786c44aadc58f740594cb0dee519d
Benedikt Zumtobel <benedikt@zumtobel.dev>
2023-06-17 01:55:14 +0200

    test: test main function

--- cmd/pwch/main_test.go
+++ cmd/pwch/main_test.go
@@ -446,3 +446,7 @@ func TestPasswordSubmitHandler(t *testing.T) {
 	// restore log output to stdout
 	log.SetOutput(os.Stdout)
 }
+
+func TestMain(t *testing.T) {
+	configPath = "../../config/config.yml"
+}
